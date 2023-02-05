d28fcf288b111b3198bfc79212100b6b071119af
Harshvardhan Sahay <harshvardhansahay5@gmail.com>
2023-02-05 12:51:55 +0530

    added files

---
+++ auto-copy-maps.bat
@@ -0,0 +1,5 @@
+:: C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\resource\overviews
+@echo off
+xcopy /v /s /q /y ".\01 default" "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\resource\overviews"
+xcopy /v /s /q /y ".\02 spectate" "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\resource\overviews"
+exit
