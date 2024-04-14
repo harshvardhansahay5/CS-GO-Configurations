121253f45077676ab5d7518200ed0381f7a5f25c
codecoffeecode <harshvardhansahay5@gmail.com>
2024-04-14 13:28:02 +0530

    Added git pull functionality to copy_autoexec.bat

--- copy_autoexec.bat
+++ copy_autoexec.bat
@@ -1,3 +1,15 @@
 @echo off
+
+:choice
+set /P c=Fetch Latest Updates From GitHub[Y/N]?
+if /I "%c%" EQU "Y" goto :gitpull
+if /I "%c%" EQU "N" goto :xcopy_code
+goto :choice
+
+:gitpull
+git pull
+
+:xcopy_code
 xcopy /v /q /y ".\autoexec.cfg" "C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\game\csgo\cfg"
+
 exit
